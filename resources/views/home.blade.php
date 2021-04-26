68efea1d3710e376a7effbb85a8dd1857092812e
hattt <Tranthuha19982015@gmail.com>
2021-04-26 13:44:23 +0700

    source code HaShopping

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.admin')
+
+@section('title')
+    <title>Trang chu</title>
+@endsection
+
+@section('content')
+
+    <div class="content-wrapper">
+    @include('partials.content_header',['name' => 'Home', 'key' => 'home'])
+
+        <div class="content">
+            <div class="container-fluid">
+                <div class="row">
+
+                    <div class="col-md-12">
+                        Trang chu
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
